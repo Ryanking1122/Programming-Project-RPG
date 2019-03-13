c9e4f5cd008b878906fcaea37d6f2aeb4cc0462b
DESKTOP-UKSEOO1\Ryan Slack <ryanruler@hotmail.com>
2019-03-13 20:02:40 +1300

    Battle System started
    Beginning classes and state machines of Battle System created

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public string name; //Character Name
+
+    public float baseHP; //Character's Base Health Point Stat
+    public float currentHP; //Character's Current Health Point Stat
+
+    public float baseMP; //Character's Base Magic Point Stat
+    public float currentMP; //Character's Current Magic Point Stat
+
+    public float baseStrength; //Character's Base Strength Stat
+    public float currentStrength; //Character's Current Strength Stat
+    public float baseDefense; //Character's Base Defense Stat
+    public float currentDefense; //Character's Current Defense Stat
+    public float baseSpeed; //Character's Base Speed Stat
+    public float currentSpeed; //Character's Current Speed Stat
+    public float baseMagic; //Character's Base Magic Stat
+    public float currentMagic; //Character's Current Magic Stat
+    public float baseEvasion; //Character's Base Evasion Stat
+    public float currentEvasion; //Character's Current Evasion Stat
+
+    public enum Type //Character Element Type
+    {
+        FIRE,
+        WATER,
+        LIGHT,
+        DARK,
+        GRASS,
+        STEEL
+    }
+}
