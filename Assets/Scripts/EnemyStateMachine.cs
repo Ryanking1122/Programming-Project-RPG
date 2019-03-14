8dfe096e4f8693ae7e75d25606cd9e9913e965b2
DESKTOP-UKSEOO1\Ryan Slack <ryanruler@hotmail.com>
2019-03-14 19:15:28 +1300

    WaitBar Utilized and Statemachines Updated
    WaitBar now working and Statemachines update Waitbar recharge and different States upon full recharge

--- Assets/Scripts/EnemyStateMachine.cs
+++ Assets/Scripts/EnemyStateMachine.cs
@@ -6,15 +6,59 @@ public class EnemyStateMachine : MonoBehaviour
 {
     public Enemy enemy;
 
+    public enum TurnState
+    {
+        PROCESSING,
+        ADDTOLIST,
+        WAITING,
+        SELECTING,
+        ACTION,
+        DEAD
+    }
+
+    public TurnState currentState;
+    private float curCooldown = 0f;
+    private float maxCooldown = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentState = TurnState.PROCESSING;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(currentState);
+        switch (currentState)
+        {
+            case (TurnState.PROCESSING):
+                UpgradeProgressBar();
+                break;
+
+            case (TurnState.ADDTOLIST):
+                break;
+
+            case (TurnState.DEAD):
+                break;
+
+            case (TurnState.SELECTING):
+                break;
+
+            case (TurnState.WAITING):
+                break;
+
+            default:
+                break;
+        }
+
+        void UpgradeProgressBar()
+        {
+            curCooldown = curCooldown + Time.deltaTime;
+            if (curCooldown >= maxCooldown)
+            {
+                currentState = TurnState.ADDTOLIST;
+            }
+        }
     }
 }
--- Assets/Scripts/MCStateMachine.cs
+++ Assets/Scripts/MCStateMachine.cs
@@ -1,20 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MCStateMachine : MonoBehaviour
 {
     public MainCharacter mainCharacter;
 
+    public enum TurnState
+    {
+        PROCESSING,
+        ADDTOLIST,
+        WAITING,
+        SELECTING,
+        ACTION,
+        DEAD
+    }
+
+    public TurnState currentState;
+    private float curCooldown = 0f;
+    private float maxCooldown = 5f;
+    public Image progressBar;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentState = TurnState.PROCESSING;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (currentState)
+        {
+            case (TurnState.PROCESSING):
+                UpgradeProgressBar();
+                break;
+
+            case (TurnState.ADDTOLIST):
+                break;
+
+            case (TurnState.DEAD):
+                break;
+
+            case (TurnState.SELECTING):
+                break;
+
+            case (TurnState.WAITING):
+                break;
+
+            default:
+                break;
+        }
+
+        void UpgradeProgressBar()
+        {
+            curCooldown = curCooldown + Time.deltaTime;
+            float calcCooldown = curCooldown / maxCooldown;
+            progressBar.transform.localScale = new Vector3(Mathf.Clamp(calcCooldown, 0, 1), progressBar.transform.localScale.y, progressBar.transform.localScale.z);
+            if(curCooldown >= maxCooldown)
+            {
+                currentState = TurnState.ADDTOLIST;
+            }
+        }
     }
 }
